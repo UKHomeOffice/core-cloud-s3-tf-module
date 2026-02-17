9a213dd78d1fa13a535f9e763c6a3dfd7393e279
Nia Ngosi <nia.ngosi@capgemini.com>
2026-02-17 10:23:06 +0000

    CCL-7090 - Updating bucket access logging logic

--- main.tf
+++ main.tf
@@ -209,11 +209,36 @@ resource "aws_s3_bucket" "logs" {
   tags   = local.common_tags
 }
 
+data "aws_iam_policy_document" "cc_logging_bucket_policy" {
+  statement {
+    principals {
+      identifiers = ["logging.s3.amazonaws.com"]
+      type        = "Service"
+    }
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.logs.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [var.account_id]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "logging" {
+  bucket = aws_s3_bucket.logs.bucket
+  policy = data.aws_iam_policy_document.cc_logging_bucket_policy.json
+}
+
 resource "aws_s3_bucket_logging" "bucket_logging" {
-  count         = var.enable_access_logs_bucket ? 1 : 0
-  bucket        = "${var.project_name}-${var.bucket_name}-${var.environment}-logs"
-  target_bucket = "${var.project_name}-${var.bucket_name}-${var.environment}"
-  target_prefix = "access-logs/"
+  bucket        = aws_s3_bucket.this.bucket
+  target_bucket = aws_s3_bucket.logs.bucket
+  target_prefix = "log/"
+  target_object_key_format {
+    partitioned_prefix {
+      partition_date_source = "EventTime"
+    }
+  }
 }
 
 data "aws_iam_policy_document" "cc_https_policy" {
